90116f6cc5fcceff83c319e7372ca32ff1c67a49
CryptoCANINE <clodcena@gmail.com>
2026-07-05 11:27:12 -0400

    updated gitignore, updated main function

--- src/main.c
+++ src/main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include "spawnsCSV.h"
 
 int main(int argc, char **argv) {
     if (argc != 2){
         fprintf(stderr, "Incorrect arguments\nUsage: %s <file>.csv\n", argv[0]);
         return 1;
     }
+
+    SpawnArray spawns;
+
+    if (loadSpawns(argv[1], &spawns) != 0){return 1;}
+    printf("Loaded %zu creature spawn defs\n", spawns.count);
     return 0;
 }
