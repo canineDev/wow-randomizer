253d2734dca520faec48504c448ed8d518bdd747
CryptoCANINE <clodcena@gmail.com>
2026-07-05 10:53:12 -0400

    updated gitignore

--- csv/main.c
+++ csv/main.c
@@ -7,7 +7,7 @@ int main(int argc, char **argv) {
     const char *csvPath = "randomizer_creature_zones.csv";
     
     if (argc >= 2) {csvPath = argv[1];}
-    printf("Loading spawn zone export: %s\n\n", csvPath);
+    printf("Loading spawn zone export: %s\n\n\n", csvPath);
     SpawnArray spawns;
     if (loadSpawnCSV(csvPath, &spawns) != 0) {return 1;}
     outZoneList(&spawns);
